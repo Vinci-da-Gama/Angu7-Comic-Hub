f8fb2c1d21688a024d958fbbfcde004e7837dd0a
Vinci-da-Game <kylingourmand@gmail.com>
2019-10-22 00:03:00 +1100

    update: thubnail with del btn

--- src/featCompos/character-with-form/character-with-form.component.ts
+++ src/featCompos/character-with-form/character-with-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Character } from '../../contracts/interfaces/charactor';
+import { Comic } from '../../contracts/models/comic';
 
 @Component({
-  selector: 'app-character-with-form',
-  templateUrl: './character-with-form.component.html',
-  styleUrls: ['./character-with-form.component.scss']
+	selector: 'app-character-with-form',
+	templateUrl: './character-with-form.component.html',
+	styleUrls: ['./character-with-form.component.scss']
 })
 export class CharacterWithFormComponent implements OnInit {
+	@Input() comicObj: Comic;
+	roles: Character[];
 
-  constructor() { }
+	constructor(
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit(): void {
+		if (this.comicObj.characters && this.comicObj.characters.length > 0 ) {
+			this.roles = this.comicObj.characters.sort((a, b) => a.numberOfRoles.localeCompare(b.numberOfRoles))
+		}
+	}
 
 }
